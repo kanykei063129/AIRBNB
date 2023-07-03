026f4911e3d42448cc6b55bd3e6726129a16dd6e
as-aijamal <a.asangazieva@gmail.com>
2023-07-03 15:48:50 +0600

    firtst commit

---
+++ src/main/java/peaksoft/house/airbnbb9/AirbnbB9Application.java
@@ -0,0 +1,13 @@
+package peaksoft.house.airbnbb9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbnbB9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirbnbB9Application.class, args);
+    }
+
+}
---
+++ src/test/java/peaksoft/house/airbnbb9/AirbnbB9ApplicationTests.java
@@ -0,0 +1,13 @@
+package peaksoft.house.airbnbb9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbB9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
