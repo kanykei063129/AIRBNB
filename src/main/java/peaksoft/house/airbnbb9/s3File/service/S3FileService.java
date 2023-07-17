e05769d79985c8f49b35a41bd3d0bcfbb66ce7f6
kanykei063129 <akjoltoikyzykanykei@gmail.com>
2023-07-17 11:27:29 +0300

    added s3File

--- src/main/java/peaksoft/house/airbnbb9/s3File/service/S3FileService.java
+++ src/main/java/peaksoft/house/airbnbb9/s3File/service/S3FileService.java
@@ -10,13 +10,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.InvalidMimeTypeException;
 import org.springframework.web.multipart.MultipartFile;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 @Service
 public class S3FileService {
-    
+
     @Value("${application.bucket.name}")
     private String bucketName;
     private AmazonS3 s3Client;
