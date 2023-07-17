6a7a9800c7fbbb187a1f28006cf0bc23ef285e5c
kanykei063129 <akjoltoikyzykanykei@gmail.com>
2023-07-17 11:22:07 +0300

    added s3File

--- src/main/java/peaksoft/house/airbnbb9/s3File/service/S3FileService.java
+++ src/main/java/peaksoft/house/airbnbb9/s3File/service/S3FileService.java
@@ -5,20 +5,18 @@ import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import com.amazonaws.util.IOUtils;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.InvalidMimeTypeException;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 @Service
-@Slf4j
 public class S3FileService {
+    
     @Value("${application.bucket.name}")
     private String bucketName;
     private AmazonS3 s3Client;
@@ -57,7 +55,7 @@ public class S3FileService {
         try (FileOutputStream fos = new FileOutputStream(convertedFile)) {
             fos.write(file.getBytes());
         } catch (IOException e) {
-            log.error("Error converting multipartFile to file", e);
+            System.out.println(e.getMessage());
         }
         return convertedFile;
     }
