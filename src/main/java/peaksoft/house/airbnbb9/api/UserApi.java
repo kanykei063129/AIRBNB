1b50e3b66ba89f44369ce13b43b423f0086111a0
HP <adiibrahimov@gmail.com>
2023-07-24 15:41:33 +0600

    getAll Users

--- src/main/java/peaksoft/house/airbnbb9/api/UserApi.java
+++ src/main/java/peaksoft/house/airbnbb9/api/UserApi.java
@@ -1,6 +1,9 @@
 package peaksoft.house.airbnbb9.api;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +15,11 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
+@Tag(name = "UserApi",description = "Controller for Users")
+@CrossOrigin(origins = "*",maxAge = 3600)
 public class UserApi {
     private final UserService userService;
+    @Operation(summary = "allUsers", description = "Available only to registered users")
     @GetMapping("getAllUsers")
     public List<UserResponse> getAllUsers() {
         return userService.getAllUsers();
