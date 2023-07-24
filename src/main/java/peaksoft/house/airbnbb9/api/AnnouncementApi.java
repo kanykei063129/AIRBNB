dd87eb437ea1841020e91be88780f978622c5560
kanykei063129 <akjoltoikyzykanykei@gmail.com>
2023-07-24 16:53:57 +0300

    announcement methods get by id

--- src/main/java/peaksoft/house/airbnbb9/api/AnnouncementApi.java
+++ src/main/java/peaksoft/house/airbnbb9/api/AnnouncementApi.java
@@ -1,5 +1,7 @@
 package peaksoft.house.airbnbb9.api;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.security.PermitAll;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -16,32 +18,36 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/announcements")
 @RequiredArgsConstructor
+@Tag(name = "Announcement Api",description = "All announcement endpoints")
 public class AnnouncementApi {
     private final AnnouncementService announcementService;
+    @Operation(summary = "GetUserById",description = "Get announcements user by id ")
     @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping("/getById/{id}")
     public List<AnnouncementResponse> getByIdAnnouncement(@PathVariable Long id) {
         return announcementService.getByIdUser(id);
     }
-
+    @Operation(summary = "getAnnouncementByID",description = "Get announcement by id ")
     @PreAuthorize("hasAnyAuthority('USER')")
     @GetMapping("/{id}")
     public AllAnnouncementResponse getAnnouncementById(@PathVariable Long id) {
         return announcementService.getByIdAnnouncement(id);
     }
-
+    @Operation(summary = "getAnnouncements",description = "Get all announcements")
     @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping("/getAll")
     public List<AnnouncementResponse> getAllAnnouncement() {
         return announcementService.getAllAnnouncements();
     }
 
+    @Operation(summary = "update By id",description = "Update announcement by id ")
     @PermitAll
     @PutMapping("/{id}")
     @PreAuthorize("hasAuthority('ADMIN')")
     public AnnouncementResponse update(@PathVariable Long id, @RequestBody @Valid AnnouncementRequest announcementRequest) {
         return announcementService.updateAnnouncement(id, announcementRequest);
     }
+    @Operation(summary = "delete Announcement By id",description = "Delete announcement by id ")
     @PreAuthorize("hasAuthority('ADMIN')")
     @DeleteMapping("/delete/{id}")
     public SimpleResponse deleteByIdAnnouncement(@PathVariable Long id) {
