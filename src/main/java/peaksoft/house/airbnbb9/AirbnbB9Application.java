3e3cc040132a9ad8138b87702223fd380b9aabe3
as-aijamal <a.asangazieva@gmail.com>
2023-07-25 20:34:00 +0600

    deployment

--- src/main/java/peaksoft/house/airbnbb9/AirbnbB9Application.java
+++ src/main/java/peaksoft/house/airbnbb9/AirbnbB9Application.java
@@ -2,12 +2,17 @@ package peaksoft.house.airbnbb9;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class AirbnbB9Application {
 
     public static void main(String[] args) {
         SpringApplication.run(AirbnbB9Application.class, args);
+    }
+    @GetMapping
+    public String greetings(){
+        return "index";
     }
 
 }
