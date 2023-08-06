02b4487d4c3126d51b9e439a09a8eb8f6f69673f
HP <adiibrahimov@gmail.com>
2023-08-06 18:15:55 +0600

    final

--- src/main/java/peaksoft/house/airbnbb9/service/UserService.java
+++ src/main/java/peaksoft/house/airbnbb9/service/UserService.java
@@ -1,9 +1,13 @@
 
 package peaksoft.house.airbnbb9.service;
 
+import peaksoft.house.airbnbb9.dto.response.SimpleResponse;
 import peaksoft.house.airbnbb9.dto.response.UserResponse;
+
 import java.util.List;
 
 public interface UserService {
     List<UserResponse> getAllUsers();
+
+    SimpleResponse deleteUserById(Long userId);
 }
