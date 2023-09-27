780e03e86be59053788bc1fab447b8d893bb0632
duisheevvva <duisheeva.aizat@icloud.com>
2023-09-27 19:06:50 +0600

    refactor feedback and latest announcement

--- src/main/java/peaksoft/house/airbnbb9/dto/response/LatestAnnouncementResponse.java
+++ src/main/java/peaksoft/house/airbnbb9/dto/response/LatestAnnouncementResponse.java
@@ -11,6 +11,7 @@ import java.util.List;
 @Builder
 @NoArgsConstructor
 public class LatestAnnouncementResponse {
+    private Long id;
     private List<String> images;
     private String title;
     private String description;
--- src/main/java/peaksoft/house/airbnbb9/repository/BookingRepository.java
+++ src/main/java/peaksoft/house/airbnbb9/repository/BookingRepository.java
@@ -1,6 +1,8 @@
 package peaksoft.house.airbnbb9.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import peaksoft.house.airbnbb9.entity.Booking;
 
@@ -9,4 +11,7 @@ import java.util.List;
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Long> {
     List<Booking> findByAnnouncementId(Long announcementId);
+
+    @Query("SELECT b from Booking b where b.user.id = :id")
+    Booking getBookingByUserId(@Param("id")Long userid);
 }
